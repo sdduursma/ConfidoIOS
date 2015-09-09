5d2cef216734e301852866a227c1e2c6f59b7eab
Rudolph van Graan <rvg@me.com>
2015-09-09 13:43:40 +0100

    Initial Commit

---
+++ IOSKeychain/IOSKeychain.h
@@ -0,0 +1,19 @@
+//
+//  IOSKeychain.h
+//  IOSKeychain
+//
+//  Created by Rudolph van Graan on 09/09/2015.
+//  Copyright (c) 2015 Curoo Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IOSKeychain.
+FOUNDATION_EXPORT double IOSKeychainVersionNumber;
+
+//! Project version string for IOSKeychain.
+FOUNDATION_EXPORT const unsigned char IOSKeychainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOSKeychain/PublicHeader.h>
+
+
